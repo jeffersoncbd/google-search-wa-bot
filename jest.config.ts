abdf0bc7bfb2f30a85c26fc8b96b4878eedcbb42
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-08-05 19:13:20 -0300

    chore: corrige configurações do Jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,9 +6,7 @@ export default {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/application.ts',
-    '!<rootDir>/src/**/index.ts',
-    '!<rootDir>/src/_domain/entities/_protocols/*',
-    '!<rootDir>/src/_domain/entities/_services/*'
+    '!<rootDir>/src/**/index.ts'
   ],
   coverageDirectory: '__tests__/coverage',
   coverageProvider: 'v8',
