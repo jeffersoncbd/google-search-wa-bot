0a0e63c8c85fc8b5325c0bf40c07f20c4ac2e981
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-08-07 17:40:58 -0300

    refactor: remove validação desnecessária

--- src/_domain/entities/CreateGroup/Entity.ts
+++ src/_domain/entities/CreateGroup/Entity.ts
@@ -9,11 +9,6 @@ export class CreateGroup {
   constructor(private openWA: CreateAGroupThroughOpenWA) {}
 
   async create(properties: CreateGroupProperties) {
-    if (!properties) {
-      throw new ValidationError(
-        'Deve ser informado as propriedades necessárias para criar um grupo'
-      )
-    }
     if (!properties.name || properties.name.length < 4) {
       throw new ValidationError('O nome do grupo deve ter pelomenos 4 letras')
     }
--- src/_domain/entities/GetGroupId/Entity.ts
+++ src/_domain/entities/GetGroupId/Entity.ts
@@ -9,11 +9,6 @@ export class GetGroupId {
   constructor(private openWA: GetGroupIdThroughOpenWA) {}
 
   async get(properties: GetGroupIdProperties) {
-    if (!properties) {
-      throw new ValidationError(
-        'Deve ser informado as propriedades necessárias para encontrar o ID de um grupo'
-      )
-    }
     if (!properties.name || properties.name.length < 4) {
       throw new ValidationError('O nome do grupo tem no mínimo 4 letras')
     }
